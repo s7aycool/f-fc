c71bb2fc6f494bba5d3d5ed536bca15246c6e4fe
Yuri Anikin <yuri.a.anikin@gmail.com>
2021-08-27 00:10:40 +0300

    Initialize Koin

---
+++ app/src/main/java/forza/home/assignment/App.kt
@@ -0,0 +1,20 @@
+package forza.home.assignment
+
+import android.app.Application
+import forza.home.assignment.misc.di.*
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(
+            )
+        }
+    }
+}
